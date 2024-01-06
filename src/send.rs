552dd09c9c7b0a9d1077ae16bfc10f7783eb97b3
jiri <jiri@jirijakes.com>
2024-01-06 12:43:17 +0000

    Add `send` module documentation
    FossilOrigin-Name: 9a937340dbf3e3e4d6a442ced6e2bedbb796494be394856edff2daf3deee957a

--- src/send.rs
+++ src/send.rs
@@ -1,3 +1,50 @@
+//! Everything for sending Silent Payments.
+//!
+//! To send a Silent Payment, create new instance of [`SilentPayment`]
+//! and add all required and available data: addresses of recipients,
+//! outpoints that will be spent by the final transaction
+//! and private keys that were used to sign these outpoints. At then
+//! end [`generate_output_scripts`](SilentPayment::generate_output_scripts)
+//! will return a list of output scripts to be included in the transaction.
+//!
+//! The output scripts are in the same order in which the recipients were added.
+//!
+//! # Example
+//!
+//! ```
+//! # use bip352::address::SilentPaymentAddress;
+//! # use bip352::send::SilentPayment;
+//! # use bitcoin::hashes::sha256d::Hash;
+//! # use bitcoin::secp256k1::{Secp256k1, SecretKey, Error};
+//! # use bitcoin::{OutPoint, ScriptBuf, Txid};
+//! let secp = Secp256k1::new();
+//! let mut sp = SilentPayment::new(&secp);
+//!
+//! // Add recipient
+//! let address = SilentPaymentAddress::from_bech32("sp1qqgste7k9hx0qftg6qmwlkqtwuy6cycyavzmzj85c6qdfhjdpdjtdgqjuexzk6murw56suy3e0rd2cgqvycxttddwsvgxe2usfpxumr70xc9pkqwv").expect("parse address");
+//! sp.add_recipient(address);
+//!
+//! // Add selected outpoints (in any order)
+//! let outpoint: OutPoint = OutPoint::null();
+//! sp.add_outpoint(outpoint);
+//!
+//! // Add private keys that sign them (in any order)
+//! let private_key: SecretKey = SecretKey::from_slice(&[1; 32])?;
+//! sp.add_private_key(private_key);
+//!
+//! // And get the output scripts (same order as recipients)
+//! let outputs: Vec<ScriptBuf> = sp.generate_output_scripts();
+//!
+//! // outputs:
+//! // [
+//! //   Script(
+//! //     OP_PUSHNUM_1
+//! //     OP_PUSHBYTES_32
+//! //     81c5bba8d342c276158972be308d869c860e3953fcabac80ddb88ff0585b9436
+//! //   )
+//! // ]
+//! # Ok::<(), Error>(())
+//! ```
 use std::collections::HashMap;
 
 use bitcoin::secp256k1::{All, Parity, PublicKey, Secp256k1, SecretKey};
